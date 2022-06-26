179f51ad0288e4dc2650d302beafa3203478e49f
foxdarts <davidtfinger@gmail.com>
2022-06-26 11:14:00 -0400

    adding a string contatenation file to the python folder

---
+++ python basic programs/madlibs.py
@@ -0,0 +1,12 @@
+#string concatenation(putting strings together)
+
+
+vtuber = "" #creates a variable for vtuber
+
+
+print("subscribe to " + vtuber) #basic and just prints
+
+print("subscribe to {}".format(vtuber)) #another great way to print the same information with a little more context puts vtuber info into the curly braces
+
+print(f"subscribe to {vtuber}") #and you guessed it. another way to print the same info. adds format before and places variable into curly braces.
+
