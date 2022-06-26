d10653e5687e6d81996535bbee03f20960dbf252
foxdarts <davidtfinger@gmail.com>
2022-06-26 11:19:32 -0400

    commenting out python lines

--- python basic programs/madlibs.py
+++ python basic programs/madlibs.py
@@ -1,12 +1,12 @@
-#string concatenation(putting strings together)
+# #string concatenation(putting strings together)
 
 
-vtuber = "" #creates a variable for vtuber
+# vtuber = "foxdarts" #creates a variable for vtuber. yes this is a youtube chanel run by me as well.. gaming stuff is fun lol
 
 
-print("subscribe to " + vtuber) #basic and just prints
+# print("subscribe to " + vtuber) #basic and just prints
 
-print("subscribe to {}".format(vtuber)) #another great way to print the same information with a little more context puts vtuber info into the curly braces
+# print("subscribe to {}".format(vtuber)) #another great way to print the same information with a little more context puts vtuber info into the curly braces
 
-print(f"subscribe to {vtuber}") #and you guessed it. another way to print the same info. adds format before and places variable into curly braces.
+# print(f"subscribe to {vtuber}") #and you guessed it. another way to print the same info. adds format before and places variable into curly braces.
 
