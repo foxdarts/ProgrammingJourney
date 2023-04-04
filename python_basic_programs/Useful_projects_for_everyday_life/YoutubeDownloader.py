78313f49816ced72fcc8db7434680ec6f65a3008
foxdarts <davidtfinger@gmail.com>
2023-04-03 20:37:42 -0400

    changed youtube downloader

--- python_basic_programs/Useful_projects_for_everyday_life/YoutubeDownloader.py
+++ python_basic_programs/Useful_projects_for_everyday_life/YoutubeDownloader.py
@@ -1,17 +1,10 @@
 #this is a program that allows you to download a video from youtube for use offline.
 
-from pytube import YouTube #python has a youtube integration! awesome
+import pytube
 
-from sys import argv #allows the system to interact with the command line to get the link we give it
+link = "https://www.youtube.com/watch?v=Etl5T1nTyZY" #place link between quotes
 
-link = argv[1] #need to grab second thing we pass into program as the first thing is the program name.  second thing will the be the link we want. must be in quotes
-
-YTVideo = YouTube(link) #gives us a variable to feed the link into
-
-#optional prints to ensure you are getting the right video
-print("Video Title: " , YTVideo.title)
-
-print("View count: ", YTVideo.views)
+YTVideo =  pytube.YouTube(link)
 
 YTD = YTVideo.streams.get_highest_resolution() #chooses the highest resolution ffrom available streams for the video
 
